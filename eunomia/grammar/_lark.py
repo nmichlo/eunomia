0256b836cf682d144fcac22bf54a2016ec901f97
Nathan Michlo <NathanJMichlo@gmail.com>
2021-01-30 11:44:53 +0200

    getting there

--- eunomia/grammar/_lark.py
+++ eunomia/grammar/_lark.py
@@ -3,6 +3,7 @@
 # ========================================================================= #
 # Helper                                                                    #
 # ========================================================================= #
+from eunomia._interpreter import interpret_expr
 
 
 def _read_relative_file(path):
@@ -27,14 +28,35 @@ PARSER = _get_lark_interpolate_parser()
 # ========================================================================= #
 
 
-def _interpolate_string(node):
+def _interpolate_string(node, usersyms=None):
     assert node.data == 'interpolate_string'
+    strings = []
+    for token in node.children:
+        if token.data == 'template_ref':
+            strings.append('<@>')
+        elif token.data == 'template_exp':
+            strings.append('<!>')
+        elif token.data == 'str':
+            assert len(token.children) == 1
+            strings.append(str(token.children[0]))
+        else:
+            raise RuntimeError(f'Unknown token encountered: {token}')
+    # if the expression is the only value in the string,
+    # then return the actual value instead.
+    if len(strings) == 1:
+        return strings[0]
+    else:
+        return ''.join(str(v) for v in strings)
 
-def _interpret_fstring(node):
+
+def _interpret_fstring(node, usersyms=None):
     assert node.data == 'interpret_fstring'
+    assert len(node.children) == 1
+    expr = str(node.children[0])
+    return interpret_expr(expr, usersyms=usersyms)
 
 
-def interpolate_string(string):
+def interpolate_string(string, usersyms=None):
     tree = PARSER.parse(string)
 
     assert tree.data == 'interpolate'
@@ -42,34 +64,35 @@ def interpolate_string(string):
     node = tree.children[0]
 
     if node.data == 'interpolate_string':
-        strings = []
-        return ''.join(strings)
+        return _interpolate_string(node, usersyms=usersyms)
     elif node.data == 'interpret_fstring':
-        pass
+        return _interpret_fstring(node, usersyms=usersyms)
     else:
         raise RuntimeError('This should never happen!')
 
-
-
-    print(tree.pretty())
-    print()
 
 
 # ========================================================================= #
 # End                                                                       #
 # ========================================================================= #
 
+def interp_str(string):
+    string = interpolate_string(string)
+    print('\033[92m', string, '\033[0m')
 
-interpolate_string('')
-interpolate_string('asdf')
-interpolate_string('asdf${a.b.c}fdaasd${a}${{1+2}}')
-interpolate_string('${a1.b2.c3}')
-interpolate_string('${a1}${{+1}}${b2}')
-interpolate_string('asdf${a1}${{+1}}${b2}')
-interpolate_string('${{1 + 2}}')
-interpolate_string('${{f"a{1}b{1+1}c"}}')
-interpolate_string('f"1+2+3"')
-interpolate_string("f'1+2+3'")
+
+interp_str('')
+interp_str('asdf')
+interp_str('asdf${a.b.c}fdaasd${a}${{1+2}}')
+interp_str('as df${a.b.c}fdaa\tsd${a}${{1+2}}')
+interp_str('as df${a.b.c}fdaansd${a}${{1+2}}')
+interp_str('${a1.b2.c3}')
+interp_str('${a1}${{+1}}${b2}')
+interp_str('asdf${a1}${{+1}}${b2}')
+interp_str('${{1 + 2}}')
+interp_str('${{f"a{1}b{1+1}c"}}')
+interp_str('f"1+2+3"')
+interp_str("f'1+2+3={1+2+3}'")
 
 
 # parse('fdsa')

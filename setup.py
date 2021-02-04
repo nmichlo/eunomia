c059aa7d46d331d01783ab1e36b1c9416aa4fe49
Nathan Michlo <NathanJMichlo@gmail.com>
2021-02-04 04:13:04 +0200

    update classifiers + twine

--- setup.py
+++ setup.py
@@ -20,8 +20,14 @@ setuptools.setup(
     long_description_content_type="text/markdown",
 
     classifiers=[
-        "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Intended Audience :: Science/Research",
     ],
 )
